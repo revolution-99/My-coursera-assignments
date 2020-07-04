0b16b29fd2caf7c4345df1b7930307fbb4afe67d
sanu3x <60106442+sanu3x@users.noreply.github.com>
2020-07-04 11:48:23 +0530

    Add files via upload

---
+++ Module4/SpeakHello.js
@@ -0,0 +1,9 @@
+(function (window){
+  var speakWord = "Hello";
+  var helloSpeaker = function speak(names){
+      console.log(speakWord + " " + names);
+      
+    }
+    window.helloSpeaker = helloSpeaker;
+  })(window);
+
